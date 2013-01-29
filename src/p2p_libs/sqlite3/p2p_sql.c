de3b20b5f21fd8ef230cc9a142d8896205618da0
Shane B <xennetwork@hotmail.com>
2013-01-29 06:46:10 -0800

    v0.1.3_pre9

--- src/p2p_libs/sqlite3/p2p_sql.c
+++ src/p2p_libs/sqlite3/p2p_sql.c
@@ -45,6 +45,7 @@ int p2pserver_sql_pubkey_exists(gchar *client_public_key_sha256, const guchar * 
 		int rc;
 		char *errmsg = 0;
 		
+		
 		rc = sqlite3_open("/home/c_dev/p2pcrypt_server/v0.1.0/src/db/client_list", &db);
 	
 		//Was the DB file successfully opened?
@@ -56,21 +57,47 @@ int p2pserver_sql_pubkey_exists(gchar *client_public_key_sha256, const guchar * 
 			g_print("\nDB opened\n");
 			const char * pubkey_exists_sql;
 				
-			pubkey_exists_sql = g_strdup_printf("SELECT `id`, `publickey_sha256`, `publickey` FROM `clients` WHERE `id` = '1' LIMIT 0,1;");
+			pubkey_exists_sql = g_strdup_printf("SELECT `id`, `publickey_sha256`, `publickey` FROM `clients` WHERE `publickey_sha256` = '%s' LIMIT 0,1;", client_public_key_sha256);
 			
-			g_print("\nABOUT TO EXECUTE SQL\n");
 			rc = sqlite3_prepare_v2(db, pubkey_exists_sql, -1, &stmt, 0);
 			
-			g_print("\nEXECUTING\n");
-			sqlite3_step(stmt);
-			printf("%s|", sqlite3_column_text(stmt, 1));
-			printf("%s|", sqlite3_column_text(stmt, 2));
+			if(rc){
+				//Failed to open DB
+				return_code = -1;
+			}
+			
+			//Just get the one row requested from the DB
+			if(return_code == 0){
+				if(sqlite3_step(stmt) == SQLITE_ROW){
+					//Extract id
+					int pubkey_local_id = sqlite3_column_int(stmt, 0);
+					g_print("%d", pubkey_local_id);
+					
+					/*
+					//Extract publickey_sha256
+					gchar * pubkey_local_sha256;
+					pubkey_local_sha256 = g_strdup_printf("%s", sqlite3_column_text(stmt, 1));
+					
+					//Extract publickey
+					gchar * pubkey_local_string;
+					pubkey = g_strdup_printf("%s", sqlite3_column_test(stmt, 2));
+					*/
+
+					//Check if id is greater than 0 if yes then return 1 if no return 0
+					if(pubkey_local_id > 0){
+						return_code = 1;
+					}else{
+						return_code = 0;
+					} 
+				}else{
+					return_code = -2;
+				}
+				
+			}
 			
 			sqlite3_finalize(stmt);
 			
 			sqlite3_close(db);
-			 
-			return_code = 1;
 		}
 	
 	
